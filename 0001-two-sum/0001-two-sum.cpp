e261a23eea217d7cc53a16c0ac387db6e30c313a
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2025-03-22 00:45:53 +0530

    Time: 7 ms (49.41%), Space: 16.2 MB (6.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,20 +3,39 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> v;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    v.push_back(i);
-                    v.push_back(j);
-                    break;
-                    break;
-                }
+        vector<int>res;
+        map<int,int>mp; //   value -> index
+        int n = nums.size();
+
+        for(int i = 0; i< n; i++){
+            
+            int remSum = target - nums[i];
+
+            if(mp.find(remSum) != mp.end()){
+                res.push_back(i);
+                res.push_back(mp[remSum]);
+
             }
+            mp[nums[i]] = i;
         }
-        return v;
+
+        return res;
+        // BRUTE FORCE
+
+        // vector<int> v;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     for (int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         if (nums[i] + nums[j] == target)
+        //         {
+        //             v.push_back(i);
+        //             v.push_back(j);
+        //             break;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return v;
     }
 };
