1454269d373c9958b8032fdbe4d29d9271665c95
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2025-04-01 22:38:28 +0530

    Time: 36 ms (7.99%), Space: 211.6 MB (5%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    long long solve(int i , vector<vector<int>>& questions,vector<long long>&result , int n){
-        
-        if(i >= n ){
-            return 0;
-        }
-
-        if(result[i] != -1){
-            return result[i];
-        }
-
-        long long take = questions[i][0] + solve(i + questions[i][1] + 1 ,questions , result , n);
-        long long not_take = solve(i+1 , questions , result , n);
-        
-        result[i] = max(take,not_take);
-
-        return result[i];
-    }
+    // BOTTOM UP APPROACH
 
     long long mostPoints(vector<vector<int>>& questions) {
-        
         int n = questions.size();
-        vector<long long>result(n , -1);
 
-        return solve(0,questions,result, n);;
+        vector<long long>v(200001 , 0);
+
+        for(int i= n-1; i>= 0; i--){
+            v[i] = max(questions[i][0] + v[i + questions[i][1] + 1] ,v[i+1]);
+        }
+
+        return v[0];
     }
+
+    // RECURSIVE APPROACH
+
+    // long long solve(int i , vector<vector<int>>& questions,vector<long long>&result , int n){
+        
+    //     if(i >= n ){
+    //         return 0;
+    //     }
+
+    //     if(result[i] != -1){
+    //         return result[i];
+    //     }
+
+    //     long long take = questions[i][0] + solve(i + questions[i][1] + 1 ,questions , result , n);
+    //     long long not_take = solve(i+1 , questions , result , n);
+        
+    //     result[i] = max(take,not_take);
+
+    //     return result[i];
+    // }
+
+    // long long mostPoints(vector<vector<int>>& questions) {
+        
+    //     int n = questions.size();
+    //     vector<long long>result(n , -1);
+
+    //     return solve(0,questions,result, n);;
+    // }
 };
