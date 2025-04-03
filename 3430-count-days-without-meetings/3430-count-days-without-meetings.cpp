10b403267201bac4bb5174b26b6b6292d143efa7
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2025-04-03 12:13:30 +0530

    Time: 65 ms (76.99%), Space: 126.6 MB (59.17%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    // USING STL METHOD  
+    //  WORKS ONLY IN SMALL TEST CASES IT GIVES TLE IN THIS TYPE OF BIG CONSTRAINTS 
+    // int countDays(int days, vector<vector<int>>& meetings) {
+    //     vector<bool> available(days + 1, true); 
+
+    //     for (auto& meeting : meetings) {
+    //         fill(available.begin() + meeting[0], available.begin() + meeting[1] + 1, false);
+    //     }
+
+    //     return count(available.begin() + 1, available.end(), true);
+    // }
+
+
+
+    // GOOD APPROACH ACCORDING TO MATH WISE 
+    
     int countDays(int days, vector<vector<int>>& meetings) {
         sort(begin(meetings),end(meetings));
         int n = meetings.size();
