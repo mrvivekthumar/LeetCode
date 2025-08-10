00fcd228252e28fef7af3e9ba03a80e1c115ee45
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2025-08-10 17:11:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (69.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int i = 0;
-        int j = 0;
+        // SLIDING WINDOW APPROACH
+
         int n = s.size();
-        int cnt = 0;
-        vector<int >v (256 ,-1);
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
 
-        if(s.size()==0)
-            return 0;
+        vector<int> mp(256, -1); // val -> index 
 
-        int maxLen = INT_MIN;
+        if(n == 0 || n == 1){
+            return n;
+        }
 
-        while(j < n){
-
-            if(v[s[j]] != -1){
-                i = max(v[s[j]]+1,i);
-            }
-            v[s[j]] = j;
-
-            int len = (j - i + 1);
-            maxLen = max(len, maxLen);
-            j++;
-
+        while(r < n){
+            if(mp[s[r]] != -1){
+                
+                if(mp[s[r]] >= l){
+                    l = mp[s[r]] + 1;
+                }
+            }    
+            int len = r - l + 1;
+            maxLen = max(maxLen , len);
+            mp[s[r]] = r;
+            r++;
         }
         return maxLen;
+
+        // SAHIL APPROACH
+
+        // int n = s.size();
+        // int i = 0;
+        // int j = 0;
+        // int cnt = 0;
+        // vector<int >v (256 ,-1);
+
+        // if(s.size()==0)
+        //     return 0;
+
+        // int maxLen = INT_MIN;
+
+        // while(j < n){
+
+        //     if(v[s[j]] != -1){
+        //         i = max(v[s[j]]+1,i);
+        //     }
+        //     v[s[j]] = j;
+
+        //     int len = (j - i + 1);
+        //     maxLen = max(len, maxLen);
+        //     j++;
+        // }
+        // return maxLen;
     }
 };
