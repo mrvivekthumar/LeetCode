6d24bf3138d315593579a7fadbb237ef847260a7
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2025-08-10 16:08:33 +0530

    Time: 34 ms (5.22%), Space: 72.9 MB (16.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,68 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
-        int sum = 0;
+    // int maxSubArray(vector<int>& nums) {
+    //     int maxSum = INT_MIN;
+    //     int sum = 0;
 
-        int n = nums.size();
+    //     int n = nums.size();
 
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
+    //     for(int i = 0; i < n; i++){
+    //         sum += nums[i];
             
-            maxSum = max(maxSum , sum);
-            
-            if(sum < 0){
-                sum = 0;
-            }
+    //         maxSum = max(maxSum , sum);
+
+    //         if(sum < 0){
+    //             sum = 0;
+    //         }
+    //     }
+    //     return maxSum;
+    // }
+
+    // Divide and concquer method
+    int divideAndConquer(int l, int r, vector<int> &arr){
+        if(l == r){
+            return arr[l];
         }
-        return maxSum;
+        int mid = (l + r) / 2;
+        int LeftSideSum = divideAndConquer(l,mid,arr);
+        int RightSideSum = divideAndConquer(mid+1, r, arr);
+        int CrossArraySum = crossSum(l,r,mid,arr);
+        
+        return max({LeftSideSum, RightSideSum, CrossArraySum});
     }
+    
+    int crossSum(int l,int r,int mid,vector<int>&arr){
+        int i = mid;
+        int leftSum = 0;
+        int leftMaxSum =INT_MIN;
+        
+        while (i >= l) {
+            leftSum += arr[i];
+            leftMaxSum = max(leftMaxSum, leftSum);
+            i--;
+        }
+        
+        int rightSum = 0;
+        int rightMaxSum = INT_MIN;
+        int j = mid+1;
+        
+        while(j <= r){
+            rightSum += arr[j];
+            rightMaxSum = max(rightMaxSum , rightSum);
+            j++;
+        }
+        
+        return leftMaxSum + rightMaxSum;
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int r = n - 1;
+        
+        return divideAndConquer(0, r,nums);
+    }
+
+
+
 };
+
