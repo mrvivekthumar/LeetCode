3843fc1dcf1d3b1e5b8dae6f1976acd4e501e18d
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2026-08-01 19:39:33 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,33 +1,54 @@
 class Solution {
 public:
+    // SORT METHOD
     // bool isAnagram(string s, string t) {
     //     sort(s.begin(),s.end());
     //     sort(t.begin(),t.end());
     //     return s == t;
     // }
 
-    // 
+    // MAP METHOD
+    // bool isAnagram(string s, string t) {
+    //     map<char,int> frq;
+    //     int slen = s.length();
+    //     int tlen = t.length();
+
+    //     if(slen != tlen){
+    //         return false;
+    //     }
+
+    //     for(int i = 0; i < slen; i++) {
+    //         frq[s[i]]++;
+    //     }
+    //     for(int i = 0; i < tlen; i++){
+    //         frq[t[i]]--;
+    //     }
+
+    //     for(auto &it : frq){
+    //         if(it.second > 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // BEST METHOD
     bool isAnagram(string s, string t) {
-        map<char,int> frq;
-        int slen = s.length();
-        int tlen = t.length();
-
-        if(slen != tlen){
+        if (s.length() != t.length())
             return false;
-        }
 
-        for(int i = 0; i < slen; i++) {
-            frq[s[i]]++;
-        }
-        for(int i = 0; i < tlen; i++){
-            frq[t[i]]--;
-        }
+        int freq[26] = {0};
 
-        for(auto &it : frq){
-            if(it.second > 0){
+        for (char c : s)
+            freq[c - 'a']++;
+
+        for (char c : t) {
+            freq[c - 'a']--;
+
+            if (freq[c - 'a'] < 0)
                 return false;
-            }
         }
+
         return true;
     }
 };
