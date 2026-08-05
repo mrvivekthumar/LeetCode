37964137b11e27a7c9948dceb53946844e4fa1a3
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2026-08-05 16:38:45 +0530

    Time: 41 ms (37.34%), Space: 72.6 MB (24.07%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,49 +1,72 @@
 class Solution {
 public:
-    int KthLargestIndex(vector<int>& nums , int left, int right){
-        int pivotValue = nums[left];
-        int pivotIndex = left;
+    // QUICK SELECT METHOD
 
-        while(left <= right){
-            if(nums[left] < pivotValue && nums[right] > pivotValue){
-                swap(nums[left],nums[right]);
-                left++;
-                right--;
-            }
-            while( left <= right && nums[left] >= pivotValue ){
-                left++;
-            }
-            while(right >= left && nums[right] <= pivotValue ){
-                right--;
-            }
-        }
-        swap(nums[pivotIndex] , nums[right]);
-        return right;
+    // int KthLargestIndex(vector<int>& nums , int left, int right){
+    //     int pivotValue = nums[left];
+    //     int pivotIndex = left;
 
-    }
+    //     while(left <= right){
+    //         if(nums[left] < pivotValue && nums[right] > pivotValue){
+    //             swap(nums[left],nums[right]);
+    //             left++;
+    //             right--;
+    //         }
+    //         while( left <= right && nums[left] >= pivotValue ){
+    //             left++;
+    //         }
+    //         while(right >= left && nums[right] <= pivotValue ){
+    //             right--;
+    //         }
+    //     }
+    //     swap(nums[pivotIndex] , nums[right]);
+    //     return right;
+
+    // }
+    // int findKthLargest(vector<int>& nums, int k) {
+    //     int n = nums.size();
+
+    //     if(n == 1){
+    //         return nums[0];
+    //     }
+
+    //     int left = 0;
+    //     int right = n-1;
+    //     int pivot = 0;
+
+    //     while(true){
+
+    //         pivot = KthLargestIndex(nums,left,right);
+
+    //         if(pivot == k-1){
+    //             break;
+    //         }else if(pivot > k-1){
+    //             right = pivot - 1;
+    //         }else{
+    //             left = pivot + 1;
+    //         }
+    //     }
+    //     return nums[pivot];
+    // }
+
+
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
+        // default max-heap 
+        // priority_queue<int> pq;
 
-        if(n == 1){
-            return nums[0];
-        }
+        // Configured as a Min-Heap 
+        // ascending order: 10 20 30 40 
 
-        int left = 0;
-        int right = n-1;
-        int pivot = 0;
+        priority_queue<int, vector<int>, greater<int>> pq; 
 
-        while(true){
+        for( auto &num : nums){
+            pq.push(num);
 
-            pivot = KthLargestIndex(nums,left,right);
-
-            if(pivot == k-1){
-                break;
-            }else if(pivot > k-1){
-                right = pivot - 1;
-            }else{
-                left = pivot + 1;
+            if(pq.size() > k){
+                pq.pop();
             }
         }
-        return nums[pivot];
+        return pq.top();
+
     }
 };
