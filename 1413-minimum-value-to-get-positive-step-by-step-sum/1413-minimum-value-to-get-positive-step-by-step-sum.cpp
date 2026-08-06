2e372896bcc3b9ab2cf2d8b76032b9294e3320a4
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2026-08-06 17:45:17 +0530

    Time: 0 ms (100%), Space: 9.9 MB (51.47%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int n = nums.size();
+        // int n = nums.size();
 
-        vector<int>prefixSum(n+1);
+        // vector<int>prefixSum(n+1);
 
-        prefixSum[0] = nums[0];
-        int minValue = prefixSum[0];
+        // prefixSum[0] = nums[0];
+        // int minValue = prefixSum[0];
 
-        for(int i = 1; i < n; i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-            minValue = min(prefixSum[i],minValue);
+        // for(int i = 1; i < n; i++){
+        //     prefixSum[i] = prefixSum[i-1] + nums[i];
+        //     minValue = min(prefixSum[i],minValue);
+        // }
+
+        // if(minValue < 0){
+        //     return abs(minValue) + 1;
+        // }
+        // return 1;
+
+        //  EFFECIANT METHOD
+        int sum = 0;
+        int minSum = 0;
+
+        for (int x : nums) {
+            sum += x;
+            minSum = min(minSum, sum);
         }
 
-        if(minValue < 0){
-            return abs(minValue) + 1;
-        }
-        return 1;
-
+        return 1 - minSum;
 
     }
 };
