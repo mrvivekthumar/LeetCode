4e4e9500c40982d8dd215c404dc20b8c70ea22a9
Vivek Thumar <121384293+mrvivekthumar@users.noreply.github.com>
2026-08-14 22:47:02 +0530

    Time: 13 ms (40.57%), Space: 14.9 MB (21.46%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>umap;
+        for(char c:s){
+            umap[c]++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(umap.find(s[i])!=umap.end()){
+                if(umap[s[i]]==1){
+                    return i;
+                }
+            }
+        }                
+        return -1;
+    }
+};
